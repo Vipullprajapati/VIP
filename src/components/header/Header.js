1ccf1dc8749f826120dc0871cb165c5d2f7ef9b3
Vipul Prajapati <vipulprajapati0070@gmail.com>
2025-05-19 20:20:34 +0000

    add fake store API

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -1,4 +1,3 @@
-import React from "react"
 import {useDispatch, useSelector} from "react-redux"
 import {toggleBtnCart, toggleSearchForm} from "../../redux/products/products.slice"
 import {FontAwesomeIcon} from "@fortawesome/react-fontawesome"
