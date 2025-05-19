360a75060e5c3d7c1f0989cae254bb4240bb3bba
Vipul Prajapati <vipulprajapati0070@gmail.com>
2025-05-19 20:53:12 +0000

    some update

--- src/components/footer/Footer.js
+++ src/components/footer/Footer.js
@@ -5,7 +5,7 @@ export function Footer() {
         <footer className='bg-black text-white'>
             <div className='max-w-[1240px] mx-auto py-2 text-sm sm:text-base'>
                 <div className='flex justify-center'>
-                    <a href='https://github.com/VolodymyrGoshko/react-shop'
+                    <a href='#'
                        target='_blank'
                        rel="noreferrer"
                        className='capitalize border-b mb-3 text-base hover:text-gray-300 transition'
@@ -14,13 +14,13 @@ export function Footer() {
                     </a>
                 </div>
                 <div className='flex justify-center flex-col items-center'>
-                    <p className='text-center'>© Copyright 2022 React Shop | All Rights Reserved</p>
-                    <a href='https://www.linkedin.com/in/volodymyr-goshko-656ab920b/'
+                    <p className='text-center'>© Copyright 2025 React Shop | All Rights Reserved</p>
+                    <a href='#'
                        target="_blank"
                        rel="noreferrer"
                        className='hover:text-gray-300 transition'
                     >
-                        Created by Volodymyr Goshko
+                        Created by Vipul Devloper
                     </a>
                 </div>
             </div>
--- src/components/loader/Loader.js
+++ src/components/loader/Loader.js
@@ -1,5 +1,3 @@
-import React from "react"
-
 export function Loader() {
     return (
         <div className='loader-box'>
